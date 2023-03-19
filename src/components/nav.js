ba3c7c68868caf65067c880945c7732ac9cee847
mosespace <kisakyemoses50@gmail.com>
2023-03-19 17:48:13 +0300

    updated nav color

--- src/components/nav.js
+++ src/components/nav.js
@@ -39,7 +39,7 @@ const StyledHeader = styled.header`
         height: var(--nav-scroll-height);
         transform: translateY(0px);
         background-color: rgba(0, 0, 0);
-        box-shadow: 0 10px 30px -10px var(--navy-shadow);
+        box-shadow: 0 10px 30px -10px #fffff;
       `};
 
     ${props =>
