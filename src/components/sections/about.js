d9055bfba630596aef18f63297498725878071ae
mosespace <kisakyemoses50@gmail.com>
2023-03-19 20:42:25 +0300

    Updated about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,21 +135,38 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Moses and I enjoy creating things that live on the internet. My
+              interest in web development started back in 2019 when I decided to try editing custom
+              freeBootstrap templates from google — turns out hacking together i personally slelf
+              taught my self about HTML &amp; CSS!
             </p>
-
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              I've just recently finished my certificate in IT from{' '}
+              <a href="https://www.uict.ac.ug/" target="_blank" rel="noreferrer">
+                UICT
+              </a>
+              , to add up to my SD2x: Data Structures and Software Design professional ceriticate i
+              obtained from{' '}
+              <a
+                href="https://courses.edx.org/certificates/f07c2f59450649e68456a1ef4edcc603/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                edx
+              </a>
+              . PennX Univesity of Pennsylvania.
+            </p>
+            <p>
+              I am a strong advocate for open source and I am always interested in working on new
+              projects with new people. Do check out my repositories and feel free to reach out on{' '}
+              <a
+                href="https://api.whatsapp.com/send?phone=256770981193&text=Hello Uncle Moses, I was on your website and descided to send you a text 😊"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Whatsapp
+              </a>
+              . or email if you would like to collaborate on any project.
             </p>
 
             <p>
